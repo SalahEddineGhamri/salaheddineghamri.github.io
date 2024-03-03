806f645c2b90e56cf75437a7c65a8e5bd542b2b1
RedRock <salaheddine.ghamri@yahoo.com>
2024-03-03 19:05:09 +0100

    toward nice blog page

--- src/pages/blog.rs
+++ src/pages/blog.rs
@@ -1,40 +1,95 @@
 use yew::prelude::*;
+use select::document::Document;
+use select::predicate::{Attr, Name, Class};
+use include_dir::{include_dir, Dir};
 
-// 2. we create a parser for index.htmls in the posts
-// 3. collect all tags
-// posts_to_view are only posts that has the selected tag
-// posts is a function that return html not a vector of htmls
+use gloo_console::log;
+use wasm_bindgen::JsValue;
 
-// output of the parser
+const POSTS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/src/posts/");
+
 struct Post {
     title: String,
     description: String,
     image: String,
     date: String,
-    tags: String,
+    tags: Vec<String>,
 }
 
-//TODO fn posts(tags, all_posts) -> html
+impl Post {
+    fn new(title: String, description: String, image: String, date: String, tags: Vec<String>) -> Self {
+        Self {
+            title,
+            description,
+            image,
+            date,
+            tags,
+        }
+    }
+}
+
+//TODO: id of a post is the name of folder
+//TODO: change the black color
+
+fn parse_posts() -> Vec<Post> {
+
+    let mut posts = Vec::new();
+    let dirs = POSTS_DIR.dirs();
+
+    for dir in dirs {
+        let option_dir_name = dir.path().to_str();
+        if let Some(dir_name) = option_dir_name {
+            if dir_name.starts_with("test"){
+                let file = dir.get_file(format!("{}/index.html", dir_name)).unwrap();
+                let document = Document::from(file.contents_utf8().unwrap());
+                let title = document.find(Name("title")).next().map(|n| n.text()).unwrap_or_default().to_string();
+                let image = document.find(Attr("property", "og:image")).next().map(|n| n.attr("content").unwrap_or_default()).unwrap_or_default().to_string();
+                let description = document.find(Attr("name", "description")).next().map(|n| n.attr("content").unwrap_or_default()).unwrap_or_default().to_string();
+                let date = document.find(Attr("name", "publish-date")).next().map(|n| n.attr("content").unwrap_or_default()).unwrap_or_default().to_string();
+                let tags = document.find(Class("post-tag")).map(|tag| tag.text()).collect::<Vec<_>>();
+
+                let post = Post{title: title,
+                                description: description,
+                                image: image,
+                                date: date,
+                                tags: tags,
+                                };
+
+                posts.push(post);
+            }
+        }
+    }
+    posts
+}
+
+//TODO make separate file
+#[function_component]
+pub fn TagSelector() -> Html {
+    html! {
+        <a class = "">{" tag1 "}</a>
+    }
+}
+
+//TODO make separate file
+#[function_component]
+pub fn PostsGrid() -> Html {
+
+    let posts: Vec<Html> = vec![];
+
+    html!{
+        <span class = "grid text-blue-500 gap-2 auto-cols-auto auto-rows-auto row-auto place-items-center">{posts}</span>
+    }
+}
 
 #[function_component]
 pub fn Blog() -> Html {
+    // tags sends the tags_name to state through callback
+    // tag_name determins posts to view
+    // send them as attrib to grib element
+    let path = "posts";
+    let base_url = "salaheddineghamri.github.io";
 
-    //TODO we need function here to define what posts to show
-    let posts_to_view: Vec<Post> = vec![
-               Post {
-                   title: "How to create something".to_string(),
-                   description: "This is coool or what ? ".to_string(),
-                   image: "/posts/images/flowers.jpg".to_string(),
-                   date: "2024-03-02".to_string(),
-                   tags: "robotics".to_string(),
-                    },
-               Post {
-                   title: "How to create something gggggggggggggggggg".to_string(),
-                   description: "This is coool or what ? ".to_string(),
-                   image: "/posts/images/flowers.jpg".to_string(),
-                   date: "2024-03-02".to_string(),
-                   tags: "robotics".to_string(),
-                    },];
+    let posts_to_view = parse_posts();
 
     let posts: Vec<Html> = posts_to_view
         .iter()
@@ -52,6 +107,7 @@ pub fn Blog() -> Html {
     html!{
     <>
         <div class = " container mx-auto self-center flex flex-col justify-self-center items-center p-8 w-3/4 md:flex-row">
+            // TODO: this a loop of tags
             <div class = "bg-white flex flex-col rounded-lg shadow-lg w-64 h-auto p-8">
                 <a class = "">{" tag1 "}</a>
                 <a class = "">{" tag2 "}</a>
